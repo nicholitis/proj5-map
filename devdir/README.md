8d64d9aaf23e5bee988cc8bcb292d091573d2aeb
nicholas eskie <neskie@septica.cs.uoregon.edu>
2017-11-01 19:18:29 -0700

    final draft

---
+++ devdir/README.md
@@ -0,0 +1,6 @@
+# Project 5: Map displaying points of interst
+
+
+##google maps api n stuff
+
+##Author: Nicholas Eskie, neskie@uoregon.edu
--- devdir/flask_map.py
+++ devdir/flask_map.py
@@ -17,7 +17,7 @@ import logging
 app = flask.Flask(__name__)
 CONFIG = config.configuration()
 app.secret_key = CONFIG.SECRET_KEY
-
+app.developer_key = CONFIG.DEVELOPER_KEY
 ###
 # Pages
 ###
@@ -27,7 +27,8 @@ app.secret_key = CONFIG.SECRET_KEY
 @app.route("/index")
 def index():
     app.logger.debug("Main page entry")
-    return flask.render_template('browserlocation.html')
+    flask.g.key = app.developer_key
+    return flask.render_template('index.html')
 
 
 @app.errorhandler(404)
@@ -35,28 +36,6 @@ def page_not_found(error):
     app.logger.debug("Page not found")
     flask.session['linkback'] = flask.url_for("index")
     return flask.render_template('404.html'), 404
-
-
-###############
-#
-# AJAX request handlers
-#   These return JSON, rather than rendering pages.
-#
-###############
-@app.route("/_map_location")
-def _map_location():
-    """
-    """
-    app.logger.debug("Got a JSON request")
-    km = request.args.get('km', 999, type=float)
-    brevet_dist = request.args.get('brevet_dist', 0, type=float)
-    date = request.args.get('date', 0, type=str)
-    time = request.args.get('time', 0, type=str)
-
-    open_time = acp_times.open_time(km, brevet_dist, brevet_start_time.isoformat())
-    close_time = acp_times.close_time(km, brevet_dist, brevet_start_time.isoformat())
-    result = {"open": open_time, "close": close_time}
-    return flask.jsonify(result=result)
 
 
 #############
@@ -68,4 +47,3 @@ if app.debug:
 if __name__ == "__main__":
     print("Opening for global access on port {}".format(CONFIG.PORT))
     app.run(port=CONFIG.PORT, host="0.0.0.0")
-
